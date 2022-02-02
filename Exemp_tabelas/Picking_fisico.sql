fa5aafe78e02e0dfec551611a639a847c6230369
André Mourão <affmourao@gmail.com>
2022-02-02 10:38:17 +0000

    backup

---
+++ Exemp_tabelas/Picking_fisico.sql
@@ -0,0 +1,20 @@
+/* Picking_logico: */
+
+CREATE TABLE Artigos (
+    ID int PRIMARY KEY,
+    Nome nvarchar,
+    Quantidade int,
+    Cod_Barras int
+);
+
+CREATE TABLE Encomendas (
+    ID int PRIMARY KEY,
+    ID_Artigos int,
+    Cod_Barras int,
+    Situacao nvarchar
+);
+ 
+ALTER TABLE Encomendas ADD CONSTRAINT FK_Encomendas_2
+    FOREIGN KEY (ID_Artigos)
+    REFERENCES Artigos (ID)
+    ON DELETE SET NULL;
