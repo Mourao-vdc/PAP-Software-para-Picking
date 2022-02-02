528c7916f3831b2029fbcd8bfaea1f0c6acc0ae5
André Mourão <affmourao@gmail.com>
2022-02-02 11:55:47 +0000

    Programa

---
+++ Software_Picking/Software_Picking/Software_Picking/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Software_Picking.ViewModels;
+using Software_Picking.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Software_Picking
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Software_Picking/Software_Picking/Software_Picking/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Software_Picking.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Software_Picking.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
