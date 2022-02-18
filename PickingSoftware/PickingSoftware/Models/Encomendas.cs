27dfd957a9772527930261846d88a352bc8db86f
André Mourão <affmourao@gmail.com>
2022-02-18 16:59:55 +0000

    Tabela Encomendas

---
+++ PickingSoftware/PickingSoftware/Models/Encomendas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlServerCe;
+
+namespace PickingSoftware.Models
+{
+    public class Encomendas
+    {
+        public int ID { get; set; }
+        public int ID_Artigos { get; set; }
+        public string Cod_Barras { get; set; }
+        public bool Situacao { get; set; }
+        public int Quant_artigos { get; set; }
+
+
+        public static List<Encomendas> GetArtigos()
+        {
+            SqlCeConnection con =
+                new SqlCeConnection(@"DataSource=|DataDirectory|\Picking.sdf");
+            con.Open();
+            string query = "SELECT * FROM Encomendas";
+            SqlCeCommand cmd = new SqlCeCommand(query, con);
+            SqlCeDataReader dr = cmd.ExecuteReader();
+
+            List<Encomendas> _tst = new List<Encomendas>();
+            while (dr.Read())
+            {
+                _tst.Add(new Encomendas
+                {
+                    ID = (int)dr["ID"],
+                    ID_Artigos = (int)dr["ID_Artigos"],
+                    Cod_Barras = dr["Cod_Barras"].ToString(),
+                    Situacao = dr["Situacao"].ToString(),
+                    Quant_artigos = (int)dr["Quant_artigos"]
+                });
+            }
+            return _tst;
+        }
+    }
+}
