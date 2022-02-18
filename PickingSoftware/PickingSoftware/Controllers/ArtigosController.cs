823a15879ce82bae1cf19319ff932aa9ab80540a
André Mourão <affmourao@gmail.com>
2022-02-18 16:06:35 +0000

    Atualização do API

---
+++ PickingSoftware/PickingSoftware/Controllers/ArtigosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace PickingSoftware.Controllers
+{
+    public class ArtigosController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetArtigos()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, Models.Artigos.GetArtigos());
+        }
+    }
+}
