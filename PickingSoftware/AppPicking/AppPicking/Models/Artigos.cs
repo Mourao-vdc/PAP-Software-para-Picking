ab49e0cdb8df8ff3315aa3614b770ef5fb942056
André Mourão <affmourao@gmail.com>
2022-02-25 17:01:43 +0000

    Start the connection to the App whit REST API

---
+++ PickingSoftware/AppPicking/AppPicking/Models/Artigos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppPicking.Models
+{
+    class Artigos
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Cod_Barras { get; set; }
+    }
+}
