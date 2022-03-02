57aab895370fe06a40153b1107b3b5099f957427
André Mourão <affmourao@gmail.com>
2022-03-02 11:16:52 +0000

    Create Datarid

---
+++ PickingSoftware/AppPicking/AppPicking/Models/Encomendas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppPicking.Models
+{
+    class Encomendas
+    {
+        public int ID { get; set; }
+        public int ID_Artigos { get; set; }
+        public string Cod_Barras { get; set; }
+        public bool Situacao { get; set; }
+        public int Quant_artigos { get; set; }
+    }
+}
