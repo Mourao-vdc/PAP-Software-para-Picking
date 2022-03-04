2a34e8970dbd4ebbe94bee30cd639b935cdf5017
André Mourão <affmourao@gmail.com>
2022-03-04 16:50:19 +0000

    Datagrid design

--- PickingSoftware/AppPicking/AppPicking/App.xaml.cs
+++ PickingSoftware/AppPicking/AppPicking/App.xaml.cs
@@ -14,7 +14,7 @@ namespace AppPicking
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
