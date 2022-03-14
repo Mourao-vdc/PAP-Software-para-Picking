6b9992643b2ee8bfb977c75b32682004587f20b5
Diogo Santiago <48964391+D1ogoX@users.noreply.github.com>
2022-03-14 15:25:56 +0000

    Criar o método de acesso à API
    Isto para ir buscar os dados à API e por sua vez a API retorna os dados da BD

--- PickingSoftware/AppPicking/AppPicking/Models/Encomendas.cs
+++ PickingSoftware/AppPicking/AppPicking/Models/Encomendas.cs
@@ -11,5 +11,15 @@ namespace AppPicking.Models
         public string Cod_Barras { get; set; }
         public bool Situacao { get; set; }
         public int Quant_artigos { get; set; }
+        
+        public static async Task<List<Encomendas>> GetEncomendas()
+        {
+            using (HttpClient _client = new HttpClient())
+            {
+                var content = await _client.GetStringAsync("https://localhost:44351/api/Encomendas/Todas");
+
+                return JsonConvert.DeserializeObject<List<Encomendas>>(content);
+            }
+        }
     }
 }
